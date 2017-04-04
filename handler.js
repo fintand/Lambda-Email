5d18dff1968d55c8e0cc9c4ee5dfaa6cfcb6d246
Fintan Dunleavy <fintan.dunleavy@gmail.com>
2017-04-04 23:01:39 +0100

    implemented recaptcha, fixed DB put

--- handler.js
+++ handler.js
@@ -6,6 +6,8 @@ const DOMAIN = process.env.DOMAIN;
 const EMAIL_FROM = process.env.EMAIL_FROM;
 const EMAIL_TO = process.env.EMAIL_TO;
 const TABLE = process.env.TABLE_NAME;
+const CAPTCHA_SECRET = process.env.CAPTCHA_SECRET;
+const request = require('superagent');
 const mailgun = require('mailgun-js')({apiKey: API_KEY, domain: DOMAIN}); // emailing service
 const mailcomposer = require('mailcomposer');
 const Hogan = require('hogan.js');
@@ -28,89 +30,102 @@ module.exports.contact = (event, context, callback) => {
   const subject = event.queryStringParameters.subject;
   const user_message = event.queryStringParameters.message;
   const email = event.queryStringParameters.email;
+  const captchaResponse = event.queryStringParameters.captchaResponse;
 
   console.log(event);
 
-  // spam protection
-  if (spam) {
-    callback(null, RESPONSES.SPAM);
-    return;
-  }
+  request
+    .get('https://www.google.com/recaptcha/api/siteverify')
+    .query({ secret: CAPTCHA_SECRET, response: captchaResponse, remoteip: event.requestContext.identity.sourceIp})
+    .set('Accept', 'application/json')
+    .end(function (err, res) {
+      if(err) {
+        console.log(err);
+        return;
+      }
+      console.log(res.text);
 
-  // check email
-  if (email) {
-    const isValidEmail = validator.validate(email);
-    if (!isValidEmail) {
-      callback(null, RESPONSES.EMAIL);
-      return;
-    }
-  }
+      // spam protection
+      if (spam) {
+        callback(null, RESPONSES.SPAM);
+        return;
+      }
 
-  if (name && number && subject && user_message) {
+      // check email
+      if (email) {
+        const isValidEmail = validator.validate(email);
+        if (!isValidEmail) {
+          callback(null, RESPONSES.EMAIL);
+          return;
+        }
+      }
 
-      const mail = mailcomposer({
-        from: EMAIL_FROM,
-        to: EMAIL_TO,
-        subject: name + '::' + subject,
-        body: 'Test email subject',
-        html: compiledTemplate.render({name: name, email: email, number: number, subject: subject, message: user_message})
-      });
+      if (name && number && subject && user_message) {
 
-      mail.build(function (mailBuildError, message) {
-
-        const dataToSend = {
+        const mail = mailcomposer({
+          from: EMAIL_FROM,
           to: EMAIL_TO,
-          message: message.toString('ascii')
-        };
+          subject: name + '::' + subject,
+          body: 'Test email subject',
+          html: compiledTemplate.render({name: name, email: email, number: number, subject: subject, message: user_message})
+        });
 
-        mailgun.messages().sendMime(dataToSend, function (sendError, body) {
-          if (sendError) {
-            console.log(sendError);
-            return;
-          }
-          const timestamp = new Date().getTime();
+        mail.build(function (mailBuildError, message) {
 
-          model.id = uuid.v1();
-          model.number = number;
-          model.name = name;
-          model.email = email;
-          model.subject = subject;
-          model.message = user_message;
-          model.createdAt = timestamp;
-          model.ip = event.requestContext.identity.sourceIp;
+          const dataToSend = {
+            to: EMAIL_TO,
+            message: message.toString('ascii')
+          };
 
-          console.log(model);
-
-          db.put({
-            TableName: TABLE,
-            Item: model,
-            Expected: {
-              number: { Exists: false }
-            }
-          }, (error, result) => {
-            // handle potential errors
-            if (error) {
-              console.error(error); // eslint-disable-line no-console
-              callback(new Error('Couldn\'t create the client item.'));
+          mailgun.messages().sendMime(dataToSend, function (sendError, body) {
+            if (sendError) {
+              console.log(sendError);
               return;
             }
+            const timestamp = new Date().getTime();
 
-            // create a response
-            const response = {
-              statusCode: 200,
-              body: JSON.stringify(result.Item),
-            };
-            callback(null, response);
+            model.id = uuid.v1();
+            model.number = number;
+            model.name = name;
+            model.email = email || null;
+            model.subject = subject;
+            model.message = user_message;
+            model.createdAt = timestamp;
+            model.ip = event.requestContext.identity.sourceIp;
+
+            console.log(model);
+
+            db.put({
+              TableName: TABLE,
+              Item: model,
+              Expected: {
+                number: { Exists: false }
+              }
+            }, (error, result) => {
+              // handle potential errors
+              if (error) {
+                console.error(error); // eslint-disable-line no-console
+                callback(new Error('Couldn\'t create the client item.'));
+                return;
+              }
+
+              // create a response
+              const response = {
+                statusCode: 200,
+                body: JSON.stringify(result.Item),
+              };
+              callback(null, response);
+            });
+
+            callback(null, RESPONSES.CREATED);
+
           });
-
-          callback(null, RESPONSES.CREATED);
-
         });
-      });
-      return;
-    }
+        return;
+      }
 
 
-  callback(null, RESPONSES.ERROR);
+      callback(null, RESPONSES.ERROR);
+    });
 
 };
